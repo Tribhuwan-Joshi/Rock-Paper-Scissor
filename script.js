75cd84d8a0e2b779c73d00fe2c97e9b054312bea
Tribhuwan Joshi <mathincosmos@gmail.com>
2022-04-16 16:50:29 +0530

    create game function to simulate the game 5 times and declare winner

--- script.js
+++ script.js
@@ -10,8 +10,7 @@ function computerPlay(){
 /* Simulate a single round of Rock Paper Scissors */
 
 function playRound(playerSelection , computerSelection){
-    playerSelection = playerSelection.toLowerCase();
-    if(playerSelection===computerSelection().toLowerCase()){
+    if(playerSelection===computerSelection.toLowerCase()){
         return `Draw !`;
     }
     else if(playerSelection==="rock" && computerSelection==="Paper"){
@@ -35,3 +34,33 @@ function playRound(playerSelection , computerSelection){
         return `You Win ! Rock beats Scissors`;
     }
 }
+
+/* Game function will call playRound function and simulate the game 5 times */
+function game(){
+    let playerPoints = computerPoints = 0;
+    for(let i =0 ; i<5; i++){
+        const computerSelection = computerPlay();
+        const playerSelection = prompt("rock or paper or scissors ?").toLowerCase();
+        if(playerSelection==="rock" || playerSelection==="scissors" || playerSelection==="paper"){
+            console.log(`You choose ${playerSelection} and Computer choose ${computerSelection}`);
+            let res = playRound(playerSelection,computerSelection);
+            console.log(res);
+
+            if(res.includes("Win"))playerPoints++ ;
+            else if(res.includes("Lose")) computerPoints++;
+            
+
+        }
+        else{
+        console.log("Please make a valid choice");
+        }
+        
+    }
+    console.log("player points ", playerPoints, "computer points " ,computerPoints);
+    (computerPoints>playerPoints)?console.log("Computer win the Game :( "):
+    (playerPoints>computerPoints)?console.log("You win the Game :) "):
+    console.log("Its a draw.");
+
+}
+
+game();
